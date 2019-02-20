44bcfa199a413eee6393f525cbf3c56b7d98e677
ncolmore@email.arizona.edu <ncolmore@email.arizona.edu>
2019-02-20 10:38:59 -0700

    Step1

---
+++ src/node.java
@@ -0,0 +1,317 @@
+import javafx.util.Pair;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.StrictMath.abs;
+
+public class node {
+    public ArrayList<Integer> state;
+    public node parent = null;
+    public Integer parentAction = 0;
+    public int gVal; //#moves so far
+    public int hManhattan;
+    public int hDisplaced; //# displaced tiles
+    public int gPlusManhattan;
+    public int gPlusHemming;
+    public int hemmingPlusManhattan;
+    public ArrayList<node> children;
+    public ArrayList <Integer> goal=new ArrayList<>(Arrays.asList(1,2,3,8,0,4,7,6,5));
+
+    boolean equalstate() {
+       for(int i=0;i<9;i++){
+           if(goal.get(i)!= state.get(i)){
+               return false;
+           }
+       }
+        return true;
+    }
+
+    public node() {
+        state = new ArrayList<>();
+        children = new ArrayList<>();
+    }
+    public node(node n1) {
+        state=n1.state;
+        parent=n1.parent;
+        parentAction=n1.parentAction;
+        gVal=n1.gVal;
+        hManhattan=n1.hManhattan;
+        hDisplaced=n1.hDisplaced;
+        gPlusManhattan=n1.gPlusManhattan;
+        gPlusHemming=n1.gPlusHemming;
+        hemmingPlusManhattan=n1.hemmingPlusManhattan;
+        children=n1.children;
+        goal=n1.goal;
+    }
+    public node(ArrayList<Integer> state) {
+        this.state = state;
+        parent = null;
+        parentAction = 0;
+        gVal=0;
+        children = new ArrayList<>();
+    }
+
+    void expand(node parent, ArrayList<Integer> prevState) {
+        ArrayList<Integer> tempState;
+        node tempNode = new node();
+        int position = prevState.indexOf(0);
+        switch (position) {
+            case 0://0 located in first position, can go down,right
+                moveRight(prevState, position);
+                moveDown(prevState, position);
+                break;
+            case 1://0 located in second position, can go down,right,left
+                moveDown(prevState, position);
+                moveLeft(prevState, position);
+                moveRight(prevState, position);
+                break;
+            case 2://0 located in third position, can go down,left
+                moveDown(prevState, position);
+                moveLeft(prevState, position);
+                break;
+            case 3://0 located in fourth position, can go down,right,up
+                moveDown(prevState, position);
+                moveRight(prevState, position);
+                moveUp(prevState, position);
+                break;
+            case 4://0 located in fourth position, can go down,right,up
+                moveDown(prevState, position);
+                moveRight(prevState, position);
+                moveUp(prevState, position);
+                moveLeft(prevState, position);
+                break;
+            case 5://0 located in fourth position, can go down,right,up
+                moveDown(prevState, position);
+                moveUp(prevState, position);
+                moveLeft(prevState, position);
+                break;
+            case 6://0 located in fourth position, can go down,right,up
+                moveUp(prevState, position);
+                moveRight(prevState, position);
+                break;
+            case 7://0 located in fourth position, can go down,right,up
+                moveRight(prevState, position);
+                moveUp(prevState, position);
+                moveLeft(prevState, position);
+                break;
+            case 8://0 located in fourth position, can go down,right,up
+                moveUp(prevState, position);
+                moveLeft(prevState, position);
+                break;
+        }
+    }
+
+    ArrayList<Integer> createArray(ArrayList<Integer> prevState) {
+        ArrayList<Integer> tempState = new ArrayList<>();
+        for (int i = 0; i < prevState.size(); i++) {
+            tempState.add(prevState.get(i));
+        }
+        return tempState;
+    }
+
+    ArrayList<Integer> swapValues(ArrayList<Integer> tempList, int indexOf0, int indexOfSwap) {
+        int tempValue = tempList.get(indexOfSwap);
+        tempList.set(indexOf0, tempValue);
+        tempList.set(indexOfSwap, 0);
+        return tempList;
+    }
+
+    void moveLeft(ArrayList<Integer> prevVal, int index) {
+        node tempNode = new node();
+        ArrayList<Integer> tempState = createArray(prevVal);
+        tempState = swapValues(tempState, index, index - 1);
+        tempNode.state = tempState;
+        node temp;
+        tempNode.parent = this;
+        tempNode.parentAction = 4;
+        tempNode.gVal=tempNode.gVal+1;
+        hamming(tempNode);
+        manhattan(tempNode);
+        calculateHeuristic(tempNode);
+        this.children.add(tempNode);
+        return;
+    }
+
+    void moveRight(ArrayList<Integer> prevVal, int index) {
+        node tempNode = new node();
+        ArrayList<Integer> tempState = createArray(prevVal);
+        tempState = swapValues(tempState, index, index + 1);
+        tempNode.state = tempState;
+        tempNode.parent = this;
+        tempNode.parentAction = 2;
+        tempNode.gVal=tempNode.gVal+1;
+        hamming(tempNode);
+        manhattan(tempNode);
+        calculateHeuristic(tempNode);
+        this.children.add(tempNode);
+        return;
+    }
+
+    void moveUp(ArrayList<Integer> prevVal, int index) {
+        node tempNode = new node();
+        ArrayList<Integer> tempState = createArray(prevVal);
+        tempState = swapValues(tempState, index, index - 3);
+        tempNode.state = tempState;
+        tempNode.parent = this;
+        tempNode.parentAction = 1;
+        tempNode.gVal=tempNode.gVal+1;
+        hamming(tempNode);
+        manhattan(tempNode);
+        calculateHeuristic(tempNode);
+        this.children.add(tempNode);
+        return;
+    }
+
+    void moveDown(ArrayList<Integer> prevVal, int index) {
+        node tempNode = new node();
+        ArrayList<Integer> tempState = createArray(prevVal);
+        tempState = swapValues(tempState, index, index + 3);
+        tempNode.state = tempState;
+        tempNode.parent = this;
+        tempNode.parentAction = 3;
+        tempNode.gVal=tempNode.gVal+1;
+        hamming(tempNode);
+        manhattan(tempNode);
+        calculateHeuristic(tempNode);
+        this.children.add(tempNode);
+        return;
+    }
+
+    void hamming(node state){
+        int hamming=0;
+        for(int i=0;i<9;i++){
+            if(!(state.state.get(i).equals(goal.get(i)))){
+                hamming++;
+            }
+        }
+        state.hDisplaced=hamming;
+    }
+
+    void manhattan(node state){
+        int manhattan=0;
+        for(int i=0;i<9;i++){
+          int position=state.state.indexOf(i);
+          int goalPosition=goal.indexOf(i);
+          int difference=abs(position-goalPosition);
+          switch(difference){
+              case 0:
+                  manhattan=manhattan+0;
+                  break;
+              case 1:
+                  manhattan++;
+                  break;
+              case 2:
+                  manhattan=manhattan+2;
+                  break;
+              case 3:
+                  manhattan++;
+                  break;
+              case 4:
+                  manhattan=manhattan+2;
+                  break;
+              case 5:
+                  manhattan=manhattan+3;
+                  break;
+              case 6:
+                  manhattan=manhattan+2;
+                  break;
+              case 7:
+                  manhattan=manhattan+3;
+                  break;
+              case 8:
+                  manhattan=manhattan+4;
+                  break;
+          }
+        }
+        state.hManhattan=manhattan;
+    }
+
+    void calculateHeuristic(node tempNode){
+        tempNode.gPlusHemming=tempNode.gVal+tempNode.hDisplaced;
+        tempNode.gPlusManhattan=tempNode.gVal+tempNode.hManhattan;
+        tempNode.hemmingPlusManhattan=tempNode.hManhattan+tempNode.hDisplaced;
+    }
+
+    public ArrayList<Integer> getState() {
+        return state;
+    }
+
+    public void setState(ArrayList<Integer> state) {
+        this.state = state;
+    }
+
+    public node getParent() {
+        return parent;
+    }
+
+    public void setParent(node parent) {
+        this.parent = parent;
+    }
+
+    public Integer getParentAction() {
+        return parentAction;
+    }
+
+    public void setParentAction(Integer parentAction) {
+        this.parentAction = parentAction;
+    }
+
+    public int getgVal() {
+        return gVal;
+    }
+
+    public void setgVal(int gVal) {
+        this.gVal = gVal;
+    }
+
+    public int gethManhattan() {
+        return hManhattan;
+    }
+
+    public void sethManhattan(int hManhattan) {
+        this.hManhattan = hManhattan;
+    }
+
+    public int gethDisplaced() {
+        return hDisplaced;
+    }
+
+    public void sethDisplaced(int hDisplaced) {
+        this.hDisplaced = hDisplaced;
+    }
+
+    public int getgPlusManhattan() {
+        return gPlusManhattan;
+    }
+
+    public void setgPlusManhattan(int gPlusManhattan) {
+        this.gPlusManhattan = gPlusManhattan;
+    }
+
+    public int getgPlusHemming() {
+        return gPlusHemming;
+    }
+
+    public void setgPlusHemming(int gPlusHemming) {
+        this.gPlusHemming = gPlusHemming;
+    }
+
+    public ArrayList<node> getChildren() {
+        return children;
+    }
+
+    public void setChildren(ArrayList<node> children) {
+        this.children = children;
+    }
+
+    public ArrayList<Integer> getGoal() {
+        return goal;
+    }
+
+    public void setGoal(ArrayList<Integer> goal) {
+        this.goal = goal;
+    }
+}
---
+++ src/solver.java
@@ -0,0 +1,178 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class solver {
+
+    public static void main(String[] args) throws IOException {
+
+        ArrayList<ArrayList<Integer>> puzzlesToSolve = new ArrayList<ArrayList<Integer>>(); //this is the arraylist of puzzles to be solved.
+        ArrayList<node> expandedNodes=new ArrayList<>();
+        for(int j=0;j<5;j++){
+            ArrayList<Integer> blank=new ArrayList<>();
+            puzzlesToSolve.add(blank);
+        }
+        File file = new File("src/input.txt");
+        Scanner read = new Scanner(file);
+        read.useDelimiter(",|\r\n");
+        int i = 0;
+        int array = 0;
+        while (read.hasNext()) {
+            for(int j=0;j<9;j++){
+                puzzlesToSolve.get(array).add(Integer.parseInt(read.next()));
+
+            }
+            array++;
+        }
+        for(i=0;i<5;i++){
+            node node1=new node(puzzlesToSolve.get(i));
+            node1.expand(node1,puzzlesToSolve.get(i)); //expand first node
+            //TODO add solve puzzle function here
+            expandedNodes.add(node1);
+            sortNodesHemming(node1.children);
+            solveByH1(node1);
+        }
+        solver.printBoards(expandedNodes);
+        printHemmingToFile(expandedNodes);
+        printManhattanToFile(expandedNodes);
+
+
+    }
+
+    private static void printBoards(ArrayList<node> expandedNodes) {
+        for(int i=0;i<expandedNodes.size();i++){
+            System.out.println("Original Position\n");
+            System.out.println(expandedNodes.get(i).state.get(0)+" "+expandedNodes.get(i).state.get(1)+" "+expandedNodes.get(i).state.get(2));
+            System.out.println(expandedNodes.get(i).state.get(3)+" "+expandedNodes.get(i).state.get(4)+" "+expandedNodes.get(i).state.get(5));
+            System.out.println(expandedNodes.get(i).state.get(6)+" "+expandedNodes.get(i).state.get(7)+" "+expandedNodes.get(i).state.get(8));
+            System.out.println("\n");
+            for(int j=0;j<expandedNodes.get(i).children.size();j++){
+                System.out.println("Node "+ j);
+                System.out.println(("# Diplaced Tiles + gVal= "+expandedNodes.get(i).children.get(j).gPlusHemming));
+                System.out.println(expandedNodes.get(i).children.get(j).state.get(0)+" "+expandedNodes.get(i).children.get(j).state.get(1)+" "+expandedNodes.get(i).children.get(j).state.get(2));
+                System.out.println(expandedNodes.get(i).children.get(j).state.get(3)+" "+expandedNodes.get(i).children.get(j).state.get(4)+" "+expandedNodes.get(i).children.get(j).state.get(5));
+                System.out.println(expandedNodes.get(i).children.get(j).state.get(6)+" "+expandedNodes.get(i).children.get(j).state.get(7)+" "+expandedNodes.get(i).children.get(j).state.get(8));
+                System.out.println("\n");
+            }
+        }
+    }
+
+    private static void sortNodesHemming(ArrayList<node> childrenNodes){
+        Collections.sort(childrenNodes, new SortbyrollHemming());
+    }
+    private static void sortNodesHemmingManhattan(ArrayList<node> childrenNodes){
+        Collections.sort(childrenNodes, new SortbyrollManhattanHemming());
+    }
+    static class SortbyrollHemming implements Comparator<node>
+    {
+        // Used for sorting in ascending order of
+        // roll number
+        public int compare(node a, node b)
+        {
+            return a.gPlusHemming - b.gPlusHemming;
+        }
+    }
+
+    private static void sortNodesManhattan(ArrayList<node> childrenNodes){
+        Collections.sort(childrenNodes, new SortbyrollManhattan());
+    }
+    static class SortbyrollManhattan implements Comparator<node>
+    {
+        // Used for sorting in ascending order of
+        // roll number
+        public int compare(node a, node b)
+        {
+            return a.gPlusManhattan - b.gPlusManhattan;
+        }
+    }
+    static class SortbyrollManhattanHemming implements Comparator<node>
+    {
+        // Used for sorting in ascending order of
+        // roll number
+        public int compare(node a, node b)
+        {
+            return a.hemmingPlusManhattan - b.hemmingPlusManhattan;
+        }
+    }
+    static void printHemmingToFile(ArrayList<node> expandedNodes) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter Writer = new PrintWriter("OutfileHeuristic1.txt", "UTF-8");
+        sortNodesHemming(expandedNodes);
+
+        for(int i=0;i<expandedNodes.size();i++){
+            Writer.println("Problem "+ (i+1) );
+            Writer.println("Original Position\n");
+            Writer.println(expandedNodes.get(i).state.get(0)+" "+expandedNodes.get(i).state.get(1)+" "+expandedNodes.get(i).state.get(2));
+            Writer.println(expandedNodes.get(i).state.get(3)+" "+expandedNodes.get(i).state.get(4)+" "+expandedNodes.get(i).state.get(5));
+            Writer.println(expandedNodes.get(i).state.get(6)+" "+expandedNodes.get(i).state.get(7)+" "+expandedNodes.get(i).state.get(8));
+            Writer.println();
+            Writer.println("Sorted List");
+            for(int j=0;j<expandedNodes.get(i).children.size();j++){
+                Writer.println("Node "+ j);
+                Writer.println(("# Diplaced Tiles + gVal= "+expandedNodes.get(i).children.get(j).gPlusHemming));
+                Writer.println(expandedNodes.get(i).children.get(j).state.get(0)+" "+expandedNodes.get(i).children.get(j).state.get(1)+" "+expandedNodes.get(i).children.get(j).state.get(2));
+                Writer.println(expandedNodes.get(i).children.get(j).state.get(3)+" "+expandedNodes.get(i).children.get(j).state.get(4)+" "+expandedNodes.get(i).children.get(j).state.get(5));
+                Writer.println(expandedNodes.get(i).children.get(j).state.get(6)+" "+expandedNodes.get(i).children.get(j).state.get(7)+" "+expandedNodes.get(i).children.get(j).state.get(8));
+                Writer.println("\n");
+
+            }
+        }
+        Writer.close();
+    }
+    static void printManhattanToFile(ArrayList<node> expandedNodes) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter Writer = new PrintWriter("OutfileHeuristic2.txt", "UTF-8");
+        sortNodesManhattan(expandedNodes);
+        for(int i=0;i<expandedNodes.size();i++){
+            Writer.println("Problem "+ (i+1) );
+            Writer.println("Original Position\n");
+            Writer.println(expandedNodes.get(i).state.get(0)+" "+expandedNodes.get(i).state.get(1)+" "+expandedNodes.get(i).state.get(2));
+            Writer.println(expandedNodes.get(i).state.get(3)+" "+expandedNodes.get(i).state.get(4)+" "+expandedNodes.get(i).state.get(5));
+            Writer.println(expandedNodes.get(i).state.get(6)+" "+expandedNodes.get(i).state.get(7)+" "+expandedNodes.get(i).state.get(8));
+            Writer.println();
+            Writer.println("Sorted List");
+            for(int j=0;j<expandedNodes.get(i).children.size();j++){
+                Writer.println("Node "+ j);
+                Writer.println(("hManhattan + gVal= "+expandedNodes.get(i).children.get(j).gPlusManhattan));
+                Writer.println(expandedNodes.get(i).children.get(j).state.get(0)+" "+expandedNodes.get(i).children.get(j).state.get(1)+" "+expandedNodes.get(i).children.get(j).state.get(2));
+                Writer.println(expandedNodes.get(i).children.get(j).state.get(3)+" "+expandedNodes.get(i).children.get(j).state.get(4)+" "+expandedNodes.get(i).children.get(j).state.get(5));
+                Writer.println(expandedNodes.get(i).children.get(j).state.get(6)+" "+expandedNodes.get(i).children.get(j).state.get(7)+" "+expandedNodes.get(i).children.get(j).state.get(8));
+                Writer.println("\n");
+
+            }
+        }
+        Writer.close();
+    }
+
+    static node solveByH1(node puzzleToSolve) {
+        boolean goalReached = false;
+        node node1 = new node();
+        ArrayList<node> graph = new ArrayList<>();
+        ArrayList<node> closed = new ArrayList<>();
+        ArrayList<node> open = new ArrayList<>();
+        open.add(puzzleToSolve);
+        graph.add(puzzleToSolve);
+        while (open.size() != 0 && goalReached == false) {
+            node1 = new node(open.get(0));
+            closed.add(node1);
+            open.remove(0);
+            goalReached = node1.equalstate();
+            if (goalReached == true) {
+                return node1;
+
+            }
+            node1.expand(node1.getParent(), node1.state);
+            System.out.println();
+
+        }
+        return node1;
+    }
+    static void solveByH2(ArrayList<node> puzzleToSolve){
+
+    }
+    static void solveByH3(ArrayList<node> puzzleToSolve){
+
+    }
+
+}
+
